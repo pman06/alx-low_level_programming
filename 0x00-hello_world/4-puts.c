738f49a010558a92c01bb16d69f523aae879eeba
Paul Shoremekun <freemeonline@gmail.com>
2022-06-02 05:28:27 -0700

    Corrected linter betty error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-
+/**
+ * main - function to print out a simplee text
+ * Return:- zero is returned
+ */
 int main(void)
 {
-	/*Best main function*/
+	/*Print out some textsc*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
