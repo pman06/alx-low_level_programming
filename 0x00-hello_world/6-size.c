77d164eb3918df1d45f13775c40989db35cd7baa
Paul Shoremekun <freemeonline@gmail.com>
2022-06-02 06:42:17 -0700

    Added script to get size of variables on local pc

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - print memory size of every variables
+ * Return: zero value
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(long));
+	printf("Size of a char: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a char: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
