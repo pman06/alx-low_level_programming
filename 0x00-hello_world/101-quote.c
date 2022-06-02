b232812667266e23a48d756d16b632f4c05e90fc
Paul Shoremekun <freemeonline@gmail.com>
2022-06-02 12:14:59 -0700

    Added a script to print a simple text using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Function to print text using putchar
+ * Return: 1
+ */
+int main(void)
+{
+	char *mychars = "and that piece of art is useful\" -\
+			 Dora Korpar, 2015-10-19\n";
+	while (*mychars)
+		putchar(*mychars++);
+	return (1);
+}
