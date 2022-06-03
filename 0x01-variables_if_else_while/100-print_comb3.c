e15f368600c441e6301995e983a370a95860d3f6
Paul Shoremekun <freemeonline@gmail.com>
2022-06-03 09:48:36 -0700

    Corrected linter warning

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i <= 8; i++)
 	{
-		for (j=i+1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i % 10 + '0');
 			putchar(j % 10 + '0');
