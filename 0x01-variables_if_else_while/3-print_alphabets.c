74ff9c42890a131d5df0c1a2e2fa843ef0cd5ff7
Paul Shoremekun <freemeonline@gmail.com>
2022-06-03 05:52:46 -0700

    Added 4 more lines to print A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - start of execution
+ * Return: zero
+ */
+
+int main(void)
+{
+	char index;
+
+	for (index = 'a'; index <= 'z'; index++)
+	{
+		putchar(index);
+	}
+	for (index = 'A'; index <= 'Z'; index++)
+	{
+		putchar(index);
+	}
+	putchar('\n');
+	return (0);
+
+}
