90d23403aff1a217753d2bd272d8e6c1fcc82398
Paul Shoremekun <freemeonline@gmail.com>
2022-06-03 07:28:31 -0700

    Added program to print 0-f for hexes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - start of execution
+ * Return: zero
+ */
+
+int main(void)
+{
+	int index,i;
+
+	for (index = 0; index <= 15; index++)
+	{
+		if (index <10)
+			putchar(index%10+'0');
+		else
+		{
+			for (i='a'; i <= 'f'; i++)
+				putchar(i);
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
