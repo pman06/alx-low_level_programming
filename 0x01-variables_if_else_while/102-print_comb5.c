f2d8b2857cdb106e3cc225d90cd9af1b9e49f32e
Paul Shoremekun <freemeonline@gmail.com>
2022-06-06 06:47:19 -0700

    Commited code to print 4 different numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - start of execution
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = i; k <= 9; k++)
+			{	
+				for ( l = j + 1; l <= 9; l++)
+				{
+					putchar(i % 10 + '0');
+					putchar(j % 10 + '0');
+					putchar(' ');
+					putchar(k % 10 + '0');
+					putchar(l % 10 + '0');
+					if ((j == 9) && (i == 8) && (k == 9) && (l == 9))
+					{
+						break;
+					}
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
