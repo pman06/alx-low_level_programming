34a237c216c611b90552e2f35e9989aa00d0af83
Paul Shoremekun <freemeonline@gmail.com>
2022-06-07 13:19:07 -0700

    Added program to print + or - value if it is over or below

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - start of execution
+ * @n: the number to be checked
+ * Return: 1 or zero
+ */
+int print_sign(int n)
+{
+	int val;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		val = 1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		val = 0;
+	}
+	if (n < 0i)
+	{
+		_putchar('-');
+		val = -1;
+	}
+	return (val);
+}
