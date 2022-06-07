bec416a4ee4fde0d6a4ab900454f79af22685409
Paul Shoremekun <freemeonline@gmail.com>
2022-06-07 14:46:16 -0700

    Added a program to print absolute values of a given number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Function to return absolute values.
+ * @i: the number to be checked
+ * Return: 1 or zero
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
