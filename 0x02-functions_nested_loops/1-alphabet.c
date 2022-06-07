5f4e40055b433dfb670a2f77c4930627ce3d8633
Paul Shoremekun <freemeonline@gmail.com>
2022-06-07 11:20:55 -0700

    Created program to print a-z using _putchar function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - start of execution
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
