1211e4c8d957a313db5a1e27ea898fc296c15bc8
Paul Shoremekun <freemeonline@gmail.com>
2022-06-08 04:22:03 -0700

    Added program to print mulriplication table of variables between 0-15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - Function to return absolute values.
+ * @n: the number to be checked
+ * Return:  of i
+ */
+void print_times_table(int n)
+{
+	int i, j, chk;
+
+	if ((n <= 15) && (n > 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (i == 0)
+					printf("0");
+				if (i != 0)
+					printf("%d", (i * j));
+				chk = i * (j + 1);
+				if (chk < 10 && j != n)
+					printf(",   ");
+				if (chk >= 10 && chk < 100 && j != n)
+					printf(",  ");
+				if (chk >= 100 && j != n)
+					printf(", ");
+			}
+		printf("\n");
+		}
+	}
+}
