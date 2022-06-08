a89bf4877287165c5a25991bd2c17dc6df3d1418
Paul Shoremekun <freemeonline@gmail.com>
2022-06-07 17:56:19 -0700

    Added program to print the values from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,23 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-	{
-		printf("%d", n);
-	}	
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			printf(", ");
 		}
 	}
 
 	if (n < 98)	
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			printf(", ");
+
 		}
 	}
-	printf("\n");
+	printf("98\n");
 }
