74535da1f1afebc68b9ee95d09f7d4fb70abd03f
Paul Shoremekun <freemeonline@gmail.com>
2022-06-07 17:14:29 -0700

    Added last add script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - start of execution
+ * @n: Value to work with
+ * @m: second value to work with
+ * Return: integer value
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - Function to return absolute values.
+ *
+ * Return:  0
+ */
+void times_table(void)
+{
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		int n = 0;
+
+		for (j = 0; j < 10; j++)
+		{	
+			_putchar('0');
+		}
+	}
+}
