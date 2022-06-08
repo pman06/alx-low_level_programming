63ff1052a05dd23234c0279cc682a63d95bb906d
Paul Shoremekun <freemeonline@gmail.com>
2022-06-08 05:54:34 -0700

    Added program to print the miltiples of 3 and 5 until 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * func - Function to return absolute values.
+ * Return:  of i
+ */
+int main(void)
+{
+	int d, sum;
+	sum = 0;
+
+	for (d = 0; d < 1024; d++)
+	{
+		if (d % 3 == 0 || d % 5== 0)
+		{
+			sum += d;
+		}
+	}
+	printf("%d\n", sum);
+}
