4512cd7e5bb922347c0e43c90ed6590ec5739eba
Paul Shoremekun <freemeonline@gmail.com>
2022-06-08 07:40:16 -0700

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Function to return absolute values.
+ * Return:  of i
+ */
+int main(void)
+{
+	long int i, j, k, prev;
+
+	j = 1;
+	k = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		prev = k;
+		printf("%ld", k);
+		if (i != 49)
+		{
+			printf(", ");
+		}
+
+		k += j;
+		j = prev;
+	}
+	printf("\n");
+	return (0);
+}
