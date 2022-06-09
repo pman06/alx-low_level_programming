661617564be1a710d46ae8a2a3cc934b92567904
Paul Shoremekun <freemeonline@gmail.com>
2022-06-09 04:34:37 -0700

    Added program to multiply 2 variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * func - Function to return products of two numbers.
+ * @a: the first value to multiply
+ * @b: the second value to multiply with
+ * Return:  product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _isupper(char c);
 int _isdigit(int c);
+int mul(int a, int b);
