7c4f8fcad5ea6e449d75bcbb25c1fe3289655416
Paul Shoremekun <freemeonline@gmail.com>
2022-06-13 07:43:01 -0700

    Added program to print the length of a string in C

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Function to return string length.
+ * @i: string
+ * Return: length os string
+ */
+int _strlen(char *i)
+{
+	int length = strlen(i);
+
+	return (length);
+}
