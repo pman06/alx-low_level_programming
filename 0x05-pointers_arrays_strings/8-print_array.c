d07f29ac50c495ab9d8e270903ab58f4bbfff07b
Paul Shoremekun <freemeonline@gmail.com>
2022-06-13 16:16:23 -0700

    Added program to print an array element up to n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Function to return absolute values.
+ * @a: the array to print
+ * @i: the number of times of arrays to print
+ * Return:  void
+ */
+void print_array(int *a, int i)
+{
+	int j;
+
+	for (j = 0; j <i; j++)
+	{
+		if (j != i - 1)
+		{
+			printf("%d, ", a[j]);
+			continue;
+		}
+		printf("%d", a[j]);
+	}
+	printf("\n");
+
+}
