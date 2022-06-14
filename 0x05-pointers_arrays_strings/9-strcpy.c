cb91293a767ebb59cc46ea563f07c0aa40b67e77
Paul Shoremekun <freemeonline@gmail.com>
2022-06-13 17:44:15 -0700

    Added program to copy content of another string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - Function to return s.
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * Return:  of i
+ */
+char *_strcpy(char *dest, char *src)
+{
+	unsigned long int i;
+
+	for (i = 0; i < strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
