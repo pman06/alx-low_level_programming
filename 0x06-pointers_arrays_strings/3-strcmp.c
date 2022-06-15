1a7c7019b42cafd29cf0b2bbc6dc824af69f8cc0
Paul Shoremekun <freemeonline@gmail.com>
2022-06-15 07:57:40 -0700

    Added a function to compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - Function to return compared stinf .
+ * @s1: the string to compare
+ * @s2: the string to compare
+ * Return:  of i
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
