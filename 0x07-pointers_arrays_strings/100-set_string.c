05ec297fbcfeed93952166d84a17635d79467d91
Paul Shoremekun <freemeonline@gmail.com>
2022-06-20 06:30:58 -0700

    Added pprogram to change the value of a  pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - Function to change pointer value
+ * @s: variable to change
+ * @to: variable to change s to
+ * Return:  of i
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
