bc880896db3922d7737ee27ea03f5bb72a1aa466
Paul Shoremekun <freemeonline@gmail.com>
2022-06-20 06:13:43 -0700

    Corrected betty flags

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,20 +3,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Function to
- * @siize: variable to
+ * print_diagsums - Function to that prints the sum of the two diagonals
+ * @a: pointer to multideimentional array
+ * @size: variable size of the array
  * Return:  of i
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
 	int sumrit, sumlft;
-	sumrit = sumlft = 0; 
 
-	for (i = 0; i < size; i ++)
+	sumrit = sumlft = 0;
+
+	for (i = 0; i < size; i++)
 	{
-			sumrit += *((a + i * size + i));
-			sumlft += *(a + (i * size) + size -1 -i);
+		sumrit += *((a + i * size + i));
+		sumlft += *(a + (i * size) + size - 1 - i);
 	}
 	printf("%d, %d\n", sumrit, sumlft);
 }
