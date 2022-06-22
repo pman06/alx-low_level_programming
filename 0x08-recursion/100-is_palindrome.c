12788dfa9ddfb4209528652099acd714ef55a207
Paul Shoremekun <freemeonline@gmail.com>
2022-06-22 16:20:32 -0700

    Added a program to check string is palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * true_palindrome - Function to check string is palindrome
+ * @s: variable string
+ * @first: variable position first in string
+ * @last: variable position last in string
+ * Return:  of i
+ */
+int true_palindrome(char *s, char first, char last)
+{
+	if (*(s + first) != *(s + last))
+		return (0);
+	if (first < last - 1)
+		return (true_palindrome(s, (first + 1), (last - 1)));
+
+	return (1);
+}
+
+/**
+ * is_palindrome - Function to check string is palindrome
+ * @s: variable string
+ * Return:  of i
+ */
+int is_palindrome(char *s)
+{
+	int lenght = strlen(s) - 1;
+
+	if (lenght == 0)
+		return (1);
+	return (true_palindrome(s, 0, lenght));
+}
