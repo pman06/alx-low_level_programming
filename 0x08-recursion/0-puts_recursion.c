b2c882fd613ce4ff027fd4648793e0488cae84f8
Paul Shoremekun <freemeonline@gmail.com>
2022-06-22 02:45:41 -0700

    Added a function to print a string then a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Function to print a string then a new line
+ * @s: variable to
+ * Return:  of i
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
