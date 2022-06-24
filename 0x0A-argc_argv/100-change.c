2dc2ca9fe7a2645e36977af244bf74a1f624520f
Paul Shoremekun <freemeonline@gmail.com>
2022-06-24 13:30:24 -0700

    Corrected less than 0

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,6 +20,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 	val = atoi(argv[1]);
 	while (val)
 	{
