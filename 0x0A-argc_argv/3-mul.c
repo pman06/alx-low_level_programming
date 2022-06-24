d41f5e068b5046bcdd316ba6173ba10f67a9e5ba
Paul Shoremekun <freemeonline@gmail.com>
2022-06-24 07:00:14 -0700

    Corrected return value in file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,14 @@
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	if (argc > 1)
-		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	{
+		int i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", i);
+	}
 	return (0);
 }
