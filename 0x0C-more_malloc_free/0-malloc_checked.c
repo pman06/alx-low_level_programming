06324daf3b2ce28580e37f37e1c3cefe9bcd1052
Paul Shoremekun <freemeonline@gmail.com>
2022-06-30 13:11:38 -0700

    Added a program to allocate memory to function arguments

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * malloc_checked - Function to return absolute values.
+ * @b: the number to be checked
+ * Return:  of i
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c = malloc(b);
+	if (!c)
+	{
+		exit(98);
+	}
+	return (c);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+MOre Malloc free functions
