7dfc2dcbcc4d48479f71e3e5f5f0e30e76716503
Paul Shoremekun <freemeonline@gmail.com>
2022-07-04 08:11:15 -0700

    Added program to initialize variables of dog struct type

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - Function to return dog values
+ * @d: dog struct
+ * @name: name variable
+ * @age: age variable
+ * @owner: owenr variable
+ * Return:  void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
