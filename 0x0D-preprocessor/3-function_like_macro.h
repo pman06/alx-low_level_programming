9ad4c956ccc78f08acb791aac491a5a1287c3dc1
Paul Shoremekun <freemeonline@gmail.com>
2022-07-04 06:31:47 -0700

    Added a program to print the absolute value of macro variable

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,8 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x < 0) ? (x * -1) : (x))
+#include <stdlib.h>
+#define ABS(x) (abs(x))
+/**
+ * #define ABS(x) ((x < 0) ? (x * -1) : (x))
+ */
 #endif
