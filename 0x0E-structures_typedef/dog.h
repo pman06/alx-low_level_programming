0a4cfe0bcbe97784547ff6ea34482eacf3a6a749
Paul Shoremekun <freemeonline@gmail.com>
2022-07-04 07:42:02 -0700

    Added dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
-#define _DOG_H
-struct dog 
+#define _DOG_H_
+/**
+ * struct dog- structure dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owener of dog
+ *
+ */
+struct dog
 {
 	char *name;
 	float age;
