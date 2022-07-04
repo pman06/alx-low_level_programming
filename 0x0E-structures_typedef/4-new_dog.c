7c2acb7d7dff6e8e5b5e2a99d6b4c28b47f24852
Paul Shoremekun <freemeonline@gmail.com>
2022-07-04 11:30:40 -0700

    Added program to crete a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - Function to return dog values
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return:  void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+
+}
