4dd6810426b9f4c343d4d7a0481dca97e449b455
Paul Shoremekun <freemeonline@gmail.com>
2022-07-04 09:47:00 -0700

    added program to print dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,15 +12,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			d->name = "(nil)";
-		printf("Name: %s\n", d->name);
-		if (d->age == '\0')
-			printf("Age: (nil)\n");
+			printf("Name: (nil)");
 		else
-			printf("Age: %f\n", d->age);
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
-			d->owner = "(nil)";
-		printf("Owner: %s\n", d->owner);
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 
 }
