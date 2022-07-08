515e9e9105d46ea67dbe613d3eccbe94f94a57c2
Paul Shoremekun <freemeonline@gmail.com>
2022-07-08 06:29:24 -0700

    Added a function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - Function to return absolute values
+ * print_numbers- Function to print numbers followed by new line
  * @separator: chracter to print between numbers
  * @n: number of variables
  * Return:  void
@@ -14,14 +14,25 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(ap, n);
-	if (separator != NULL)
+	if (separator == NULL)
+	{
 		for (i = 0; i < n; i++)
-			printf("%i%s ", va_arg(ap, int), separator);
+			printf("%i", va_arg(ap, int));
+	}
 	else
+	{
 		for (i = 0; i < n; i++)
-			printf("%i ", va_arg(ap, int));
-	printf("\n");
+			if (i != n - 1)
+				printf("%i%s", va_arg(ap, int), separator);
+			else
+				printf("%i", va_arg(ap, int));
+	}
 	va_end(ap);
-
+	printf("\n");
 }
