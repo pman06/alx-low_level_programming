dcb71d8343d4dc324daec6363734ef4d4a1f8b92
Paul Shoremekun <freemeonline@gmail.com>
2022-07-08 05:07:07 -0700

    Added dprogram tosum varuiadic numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Function to return absolute values
+ * @count: the number to be checked
+ * Return:  int
+ */
+int sum_them_all(const unsigned int count, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start (ap, count);
+	sum = 0;
+	for (i =0; i < count; i++)
+		sum += va_arg(ap, int);
+
+	va_end (ap);
+	return (sum);
+
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,2 @@
+
+int sum_them_all(const unsigned int n, ...);
