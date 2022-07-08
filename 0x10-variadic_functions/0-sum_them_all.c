ebadec2d5f38b467d36825545984c62334da00e2
Paul Shoremekun <freemeonline@gmail.com>
2022-07-08 05:14:49 -0700

    Added dprogram tosum varuiadic numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,12 +13,12 @@ int sum_them_all(const unsigned int count, ...)
 	va_list ap;
 	unsigned int i, sum;
 
-	va_start (ap, count);
+	va_start(ap, count);
 	sum = 0;
-	for (i =0; i < count; i++)
+	for (i = 0; i < count; i++)
 		sum += va_arg(ap, int);
 
-	va_end (ap);
+	va_end(ap);
 	return (sum);
 
 }
