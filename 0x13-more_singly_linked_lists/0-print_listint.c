e89c1f52cbfb16f9030bdf57421c142a7a3a7341
Paul Shoremekun <freemeonline@gmail.com>
2022-07-18 12:32:21 -0700

    Added a function that prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint - Function add node to end of linked list
+ * @h: the pointer of pointer to the previous node
+ * Return:  size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	int lenght = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		lenght++;
+		h = h->next;
+	}
+
+	return (lenght);
+}
