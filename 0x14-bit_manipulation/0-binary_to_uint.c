2fc39fe05a8f8952418c1efff7953ba3a7478505
Paul Shoremekun <freemeonline@gmail.com>
2022-07-21 17:39:10 -0700

    Added function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_unint- Function add node to end of linked list
+ * @b: the pointer binary to convert to unsigned int
+ * Return:  unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
+
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
